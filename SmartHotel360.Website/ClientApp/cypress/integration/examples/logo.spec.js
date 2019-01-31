e3c414d054fc4923e3b7fce767abcc08b393a29d
Pooja Soude <Pooja.Soude@emtecinc.com>
2019-01-31 07:06:22 +0000

    Updated logo.spec.js

--- SmartHotel360.Website/ClientApp/cypress/integration/examples/logo.spec.js
+++ SmartHotel360.Website/ClientApp/cypress/integration/examples/logo.spec.js
@@ -1,7 +1,9 @@
-it('has a visible smarthotel logo', function() {
-  //cy.get('.sh-auth-picture').should('be.visible');
- // cy.title().should('eq', 'SmartHotel360');
-  //cy.title().should('contain', 'SmartHotel360');
-  //cy.contains('ul', 'Smart Room');
-//cy.contains('ul', 'Conference Room');
+describe('SmartHotel home page', function() {
+  beforeEach(function() {
+    cy.visit('https://smarthotel360demo.azurewebsites.net/');
+  });
+  
+  it('contains "SmartHotel360" in the title', function() {
+     cy.title().should('contain', 'SmartHotel360');
+  });
 });
