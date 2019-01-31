ca2ae14feed98ec701e828e0087414301c8460a2
Pooja Soude <Pooja.Soude@emtecinc.com>
2019-01-31 09:05:42 +0000

    Updated navigation.spec.js

--- SmartHotel360.Website/ClientApp/cypress/integration/examples/navigation.spec.js
+++ SmartHotel360.Website/ClientApp/cypress/integration/examples/navigation.spec.js
@@ -1,1 +1,9 @@
-cy.contains('Smart Room');
+describe('SmartHotel home page', function() {
+  beforeEach(function() {
+    cy.visit('https://smarthotel360demo.azurewebsites.net/');
+  });
+  
+  it('contains "SmartHotel360" in the title', function() {
+     cy.contains('Smart Room');
+  });
+});
