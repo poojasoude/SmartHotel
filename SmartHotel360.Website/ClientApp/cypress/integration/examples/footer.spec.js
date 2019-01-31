08b58252173839b4c4d0b4e8fe2f22b9c4be8001
Pooja Soude <Pooja.Soude@emtecinc.com>
2019-01-31 10:09:21 +0000

    Updated footer.spec.js

--- SmartHotel360.Website/ClientApp/cypress/integration/examples/footer.spec.js
+++ SmartHotel360.Website/ClientApp/cypress/integration/examples/footer.spec.js
@@ -1,1 +1,11 @@
 cy.get('.article').find('footer')
+
+describe('SmartHotel footer ', function() {
+  beforeEach(function() {
+    cy.visit('https://smarthotel360demo.azurewebsites.net/');
+  });
+  
+  it('contains footer', function() {
+   cy.get('.sh-footer').find('Terms & Conditions')
+  });
+});
